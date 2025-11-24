1aa0cd789bf5721444f4ba166744753a25924767
connorsnelgrove28 <connorsnelgrove28@gmail.com>
2025-11-24 16:27:02 -0500

    Add set auto-create, pagination for sets and set cards, update import script

--- server/app/models/set.py
+++ server/app/models/set.py
@@ -16,3 +16,36 @@ class Set(db.Model):
 
     def __repr__(self):
         return f"<Set {self.year} {self.brand} {self.set_name}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "sport": self.sport,
+            "year": self.year,
+            "brand": self.brand,
+            "set_name": self.set_name,
+        }
+
+    @classmethod
+    def get_or_create(cls, sport: str, year: int, brand: str, set_name: str):
+        """
+        Find an existing set with this sport/year/brand/set_name,
+        or create it (without committing yet).
+        """
+        instance = cls.query.filter_by(
+            sport=sport,
+            year=year,
+            brand=brand,
+            set_name=set_name,
+        ).first()
+
+        if instance is None:
+            instance = cls(
+                sport=sport,
+                year=year,
+                brand=brand,
+                set_name=set_name,
+            )
+            db.session.add(instance)
+
+        return instance
