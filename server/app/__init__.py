0eb5204d73184fdca69ff63ffed188a2d9962f4d
connorsnelgrove28 <connorsnelgrove28@gmail.com>
2025-11-25 17:42:00 -0500

    Add navbar + layout + cards page + auth integration

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -6,6 +6,7 @@ from .api.cards import cards_bp
 from .api.sets import sets_bp
 from .api.wanted_cards import wanted_cards_bp
 from .api.owned_cards import owned_cards_bp
+from flask_cors import CORS
 
 
 def create_app():
@@ -14,8 +15,20 @@ def create_app():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SECRET_KEY"] = "dev-secret-change-me"
 
+    CORS(
+        app,
+        resources={r"/api/*": {
+            "origins": [
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+            ]
+        }},
+        supports_credentials=True,
+    )
+
     db.init_app(app)
 
+    # register blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(cards_bp)
     app.register_blueprint(sets_bp)
